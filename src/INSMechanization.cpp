c3467f00bc68ec67550b5869ea6753b5b72fee3a
Talha <talhayyc42@gmail.com>
2025-04-01 18:30:22 -0600

    read data and basic setuo

---
+++ src/INSMechanization.cpp
@@ -0,0 +1,52 @@
+#include "INSMechanization.h"
+#include <iostream>
+#include <fstream>
+
+INSMechanization::INSMechanization() : initLatitude(0.0), initLongitude(0.0), initHeight(0.0),
+                                       initVelocity(Eigen::Vector3d::Zero()), initEulerAngles(Eigen::Vector3d::Zero())
+{
+    // Constructor initilization of the INS
+}
+
+void INSMechanization::setInitialState(double latitiude, double longitude, double height,
+                                       const Eigen::Vector3d &velocity,
+                                       const Eigen::Vector3d &eulerAngles)
+{
+    initLatitude = latitiude;
+    initLongitude = longitude;
+    initHeight = height;
+    initVelocity = velocity;
+    initEulerAngles = eulerAngles;
+}
+
+bool INSMechanization::readIMUData(const std::string &filename)
+{
+    std::ifstream file(filename, std::ios::binary);
+    if (!file)
+    {
+        std::cerr << "Error:Cannot open file" << filename << std::endl;
+        return false;
+    }
+    IMURecord record;
+
+    while (file.read(reinterpret_cast<char *>(&record), sizeof(IMURecord)))
+    {
+        imuData.push_back(record);
+    }
+    file.close();
+    std::cout << "Read " << imuData.size() << " records from" << filename << " \n";
+    return true;
+}
+
+void INSMechanization::run()
+{
+    std::cout << "Running INS Mechanization...." << std::endl;
+    std::cout << "Initial Latitude (rad): " << initLatitude << std::endl;
+    std::cout << "Initial Longitude (rad): " << initLongitude << std::endl;
+    std::cout << "Initial Height(m): " << initHeight << std::endl;
+    std::cout << "Number of IMU records: " << imuData.size() << std::endl;
+}
+
+void INSMechanization::printResulst() const
+{
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,51 @@
 // src/main.cpp
 #include <iostream>
 #include "INSMechanization.h"
+#include <Eigen/Dense>
+#include <cmath>
+#include <filesystem>
 
-int main() {
-    std::cout << "Local-Level INS Mechanization Project\n";
+int main()
+{
+    INSMechanization ins;
+    // ---------------------------------------------------------------------------
+    // Set initial conditions (provided in the project document)
+    // ---------------------------------------------------------------------------
+
+    double initLat_deg = 51.07995352;
+    double initLong_deg = -114.13371127;
+    double initHeight = 1118.502;
+
+    // convert degress to radians
+    const double deg2rad = M_PI / 180.0;
+    double initLat_rad = initLat_deg * deg2rad;
+    double initLong_rad = initLong_deg * deg2rad;
+
+    // Initial velocity is zero for a static test
+    Eigen::Vector3d initVelocity(0.0, 0.0, 0.0);
+    Eigen::Vector3d initEulerAngles(0.0, 0.0, 0.0);
+
+    // Set the initial state in the INS object
+    ins.setInitialState(initLat_rad, initLong_rad, initHeight, initVelocity, initEulerAngles);
+
+    // ---------------------------------------------------------------------------
+    // Read the IMU binary data
+    // ---------------------------------------------------------------------------
+
+    if (!ins.readIMUData("../data/project_data.BIN"))
+    {
+        std::cerr << "Failed to read IMU data." << std::endl;
+        return 1;
+    }
+    // ---------------------------------------------------------------------------
+    // Run the INS mechanization process
+    // ---------------------------------------------------------------------------
+    ins.run();
+
+    // ---------------------------------------------------------------------------
+    // Print the results
+    // ---------------------------------------------------------------------------
+    ins.printResulst();
+
     return 0;
 }
